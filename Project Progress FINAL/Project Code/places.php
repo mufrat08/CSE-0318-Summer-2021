78ac3e17cf1d0043c6969090509ab8415bb76754
Khondkar Md. Mufrat Tasif <53010210+mufrat08@users.noreply.github.com>
2021-09-22 01:18:04 +0600

    Create places.php

---
+++ Project Progress FINAL/Project Code/places.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+        <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans&display=swap" rel="stylesheet"> 
+</head>
+<body>
+<nav class="navbar navbar-expand-lg bg-success navbar-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="index.php">Travelling with Planet Plus</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+         <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
+             <li class="nav-item">
+               <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+             </li>
+             <li class="nav-item">
+                <a class="nav-link" href="about.php">Bandorban</a>
+             </li>
+             <li class="nav-item dropdown">
+                 <a class="nav-link dropdown-toggle" href="places.php" id="navbardrop" data-toggle="dropdown">Attractive Spots</a>
+               <div class="dropdown-menu">
+                     <a class="dropdown-item" href="about.php">Thanchi</a>
+                     <a class="dropdown-item" href="about.php">Amiakhum</a>
+                     <a class="dropdown-item" href="about.php">Nafakhum</a>
+               </div>
+            </li>
+             <li class="nav-item">
+                <a class="nav-link" href="gallery.php">Gallery</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="userinfo.php">Contact Us</a>
+            </li>
+         </ul>
+          
+       </div>
+  </div>
+</nav> 
+
+
+
+<section class="my-5">
+           <div class="py-4">
+              <h2 class="text-center text-success"> Attractive spots </h2>
+           </div>
+           <div class="contain-fluid">
+               <div class="row">
+                   <div class="col-lg-4 col-md-4 col-12">
+                      <div class="card">
+            <img class="card-img-top" src="images/thanchi.jpg" alt="Card image">
+                           <div class="card-body">
+                             <h4 class="card-title">Thanchi</h4>
+                              <p class="card-text">The popular Place.One such path leads to it .One of the remotest parts of our country. if u visit here you'll enjoy.</p>
+                              <a href="about.php" class="btn btn-primary">See More</a>
+                           </div>
+                      </div>
+                   </div>
+                   <div class="col-lg-4 col-md-4 col-12">
+                      <div class="card">
+            <img class="card-img-top" src="images/amiakhum112.jpg" alt="Card image">
+                           <div class="card-body">
+                             <h4 class="card-title">Amiakhum Zhorna</h4>
+                              <p class="card-text">  It is one of the most inaccessible places  in  Bangladesh.
+                              A  stream  of  cool water is   coming down at a
+                              tremendous speed, spreading milky  white  foam   over the green hills. just a beautiful place </p>
+                              <a href="about.php" class="btn btn-primary">See More</a>
+                           </div>
+                      </div>
+                   </div>
+                   <div class="col-lg-4 col-md-4 col-12">
+                      <div class="card">
+            <img class="card-img-top" src="images/nafakhum3.jpg" alt="Card image">
+                           <div class="card-body">
+                             <h4 class="card-title">Nafakhum Zhorna </h4>
+                              <p class="card-text">Sagu the river which flowing exotic waterfall call Nafakhum. It is among the largest waterfalls in the country by volume of water falling.</p>
+                              <a href="about.php" class="btn btn-primary">See More</a>
+                           </div>
+                      </div>
+                   </div>
+               </div>
+           </div> 
+       </section>
+
+        <footer>
+          <p class="p-3 bg-dark text-white text-center"> @planetplus</p>
+        </footer>
+</body>
+
+</html>
