6d988fdca7bb3ce014ea141e99fc3197cd720c63
Khondkar Md. Mufrat Tasif <53010210+mufrat08@users.noreply.github.com>
2021-09-22 01:20:49 +0600

    Create about.php

---
+++ Project Progress FINAL/Project Code/about.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+        <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans&display=swap" rel="stylesheet"> 
+</head>
+<body>
+<nav class="navbar navbar-expand-lg bg-success navbar-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="index.php">Travelling with Planet Plus</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+         <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
+             <li class="nav-item">
+               <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+             </li>
+             <li class="nav-item">
+                <a class="nav-link" href="about.php">Bandorban</a>
+             </li>
+             <li class="nav-item dropdown">
+                 <a class="nav-link dropdown-toggle" href="places.php" id="navbardrop" data-toggle="dropdown">Attractive Spots</a>
+               <div class="dropdown-menu">
+                     <a class="dropdown-item" href="about.php">Thanchi</a>
+                     <a class="dropdown-item" href="about.php">Amiakhum</a>
+                     <a class="dropdown-item" href="about.php">Nafakhum</a>
+               </div>
+            </li>
+             <li class="nav-item">
+                <a class="nav-link" href="gallery.php">Gallery</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="userinfo.php">Contact Us</a>
+            </li>
+         </ul>
+                 </div>
+  </div>
+</nav> 
+
+
+<section class="my-5">
+           <div class="py-5">
+              <h1 class="text-center text-success"> Bandarban </h1>
+           </div>
+        <div class="container-fluid">
+            <div class="row">
+               <div class="col-lg-6 col-md-6 col-12">
+                   <img src="images/tajindong peak.jpg" class="img-fluid aboutimg">     
+               </div>
+               <div class="col-lg-6 col-md-6 col-12">
+                   <h2> Hello Bandarban </h2> 
+                      <p class="py-3">Bandarban is regarded as one of the most attractive travel destinations in Bangladesh.
+                       It also is the administrative headquarters of Bandarban district, which has turned into 
+                       one of the most exotic tourist attractions in Bangladesh. 
+                       Bandarban district was established in 1983.It consists of 7 upazilas,32 unions,140 mouzas,
+                       1482 villages, 1 municipality, 10 wards and 62 mahallas. Bandarban , also called Rwadawmro in 
+                       the Marma and Arakanese languages, is a district in South-Eastern Bangladesh, and a part of 
+                       the Chittagong Division. It is one of the three hill districts of Bangladesh . 
+                       There are 28 tourist attractions found in Bandarban. the top tourists spots are keokradong
+                       pahar, amiakum zhorna, nafakhum, alikadam (dim pahar),Chimbuk pahar, Nilachol, Bogalake, Nilgiri, 
+                       sat vai khum, thanchi, debota pahar etc. </P>
+                       
+                     <h4> Thanchi </h4> 
+                       <p class="py-1">One such path leads to Thanchi, one of the remotest parts of our country. 
+                        It falls under the Bandarban District of Chittagong Hill Tracts (CHT) and it appears as a tiny speck on the map of Bangladesh.
+                        </P>
+
+                     <h4> Amiakhum Zhorna </h4> 
+                       <p class="py-1">Amiakhum Waterfall is one of the most beautiful waterfalls in Bangladesh and an excellent place to travel.
+                       Every traveler should have a tour plan to visit this beautiful tourism spot of Bandarban at least once and then they will
+                       feel to visit that travel spot again and again.
+                       </P>
+
+                     <h4> Nafakhum Zhorna </h4> 
+                        <p class="py-1">One such path leads to Thanchi, one of the remotest parts of our country. 
+                        It falls under the Bandarban District of Chittagong Hill Tracts (CHT) and it appears as a tiny speck on the map of Bangladesh.
+                        </P>
+                        
+               </div>
+            </div>
+        </div>
+      </section> 
+
+      <footer>
+          <p class="p-3 bg-dark text-white text-center"> @planetplus</p>
+        </footer>
+
+</body>
+
+</html>
