924be95d4639ed45a9bb9faaca85f2552e872cfa
Khondkar Md. Mufrat Tasif <53010210+mufrat08@users.noreply.github.com>
2021-09-22 01:20:07 +0600

    Create gallery.php

---
+++ Project Progress FINAL/Project Code/gallery.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+        <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans&display=swap" rel="stylesheet"> 
+</head>
+<body>
+<nav class="navbar navbar-expand-lg bg-success navbar-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="index.php">Travelling with Planet Plus</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+         <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
+             <li class="nav-item">
+               <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+             </li>
+             <li class="nav-item">
+                <a class="nav-link" href="about.php">Bandorban</a>
+             </li>
+             <li class="nav-item dropdown">
+                 <a class="nav-link dropdown-toggle" href="places.php" id="navbardrop" data-toggle="dropdown">Attractive Spots</a>
+               <div class="dropdown-menu">
+                     <a class="dropdown-item" href="about.php">Thanchi</a>
+                     <a class="dropdown-item" href="about.php">Amiakhum</a>
+                     <a class="dropdown-item" href="about.php">Nafakhum</a>
+               </div>
+            </li>
+             </li>
+             <li class="nav-item">
+                <a class="nav-link" href="gallery.php">Gallery</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="userinfo.php">Contact Us</a>
+            </li>
+         </ul>
+          
+       </div>
+  </div>
+</nav> 
+
+
+<section class="my-5">
+             <div class="py-4">
+                <h2 class="text-center text-success"> Gallery </h2>
+             </div>
+               <div class="contain-fluid">
+                 <div class="row">
+                   <div class="col-lg-4 col-md-4 col-12"> 
+                   <h5 class="card-text-"> Amiakhum Jhorna</h5> 
+                      <img src="images/amiakhum112.jpg" class="img-fluid pb-3">  
+                   </div> 
+                   <div class="col-lg-4 col-md-4 col-12">  
+                   <h5 class="card-text-"> Nilachol</h5>    
+                   <img src="images/nilachol114.jpg" class="img-fluid pb-3 ">
+                   </div>
+
+                   <div class="col-lg-4 col-md-4 col-12">
+                   <h5 class="card-text-"> Royado Rong </h5>   
+                      <img src="images/royadorong117.jpg" class="img-fluid pb-3">  
+                   </div>
+
+                  <div class="col-lg-4 col-md-4 col-12">  
+                   <h5 class="card-text-"> Nafakhum Jhorna</h5> 
+                      <img src="images/nafakhumjhorna120.jpg" class="img-fluid pb-3">  
+                   </div>
+
+                   <div class="col-lg-4 col-md-4 col-12">
+                   <h5 class="card-text-"> Nilgiri</h5>   
+                      <img src="images/nilgiri116.jpg" class="img-fluid pb-3">  
+                   </div>
+
+                   <div class="col-lg-4 col-md-4 col-12">
+                   <h5 class="card-text-"> Tajindong </h5>  
+                      <img src="images/tajindong peak.jpg" class="img-fluid pb-3">  
+                   </div>
+
+                   <div class="col-lg-4 col-md-4 col-12">  
+                   <h5 class="card-text-"> Sunset Time </h5> 
+                      <img src="images/sunsettimebandorban118.jpg" class="img-fluid pb-3">  
+                   </div>
+
+                   <div class="col-lg-4 col-md-4 col-12"> 
+                   <h5 class="card-text-"> Unknown Hill</h5>  
+                      <img src="images/bandorban115.jpg" class="img-fluid pb-3">  
+                   </div>
+
+                   <div class="col-lg-4 col-md-4 col-12">  
+                   <h5 class="card-text-"> Tolabong Jhorna</h5> 
+                      <img src="images/tolabongzhorna119.jpg" class="img-fluid pb-3">  
+                   </div>
+                   
+                   
+                 </div>
+               </div>
+
+        </section>
+
+
+        <footer>
+          <p class="p-3 bg-dark text-white text-center"> @planetplus</p>
+        </footer>
+</body>
+
+</html>
