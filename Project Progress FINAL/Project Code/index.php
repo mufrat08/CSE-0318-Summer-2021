3a6385b6b3593606a43891108eae73dbb38c1389
Khondkar Md. Mufrat Tasif <53010210+mufrat08@users.noreply.github.com>
2021-09-22 02:50:15 +0600

    Create index.php

---
+++ Project Progress FINAL/Project Code/index.php
@@ -0,0 +1,254 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+        <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans&display=swap" rel="stylesheet"> 
+</head>
+<body>
+<nav class="navbar navbar-expand-lg bg-success navbar-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="index.php">Travelling with Planet Plus</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+         <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+               <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+            </li>
+            
+            <li class="nav-item">
+                <a class="nav-link" href="about.php">Bandorban</a>
+            </li>
+            <li class="nav-item dropdown">
+                 <a class="nav-link dropdown-toggle" href="places.php" id="navbardrop" data-toggle="dropdown">Attractive Spots</a>
+               <div class="dropdown-menu">
+                     <a class="dropdown-item" href="about.php">Thanchi</a>
+                     <a class="dropdown-item" href="about.php">Amiakhum</a>
+                     <a class="dropdown-item" href="about.php">Nafakhum</a>
+               </div>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="gallery.php">Gallery</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="userinfo.php">Contact Us</a>
+            </li>
+         </ul>
+          
+      </div>
+  </div>
+</nav> 
+<div id="demo" class="carousel slide" data-ride="carousel">
+
+  <!-- Indicators -->
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+  </ul>
+  
+  <!-- The slideshow -->
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="images/cc1.jpg" alt="Thanchi" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>Thanchi</h3>
+        <p>We had such a great time in Thanchi!</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="images/nilgiri33.jpg" alt="Nilgiri" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>Nilgiri</h3>
+        <p>Thank You , Nilgiri !</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="images/nafa1.jpg" alt="Nafakhum" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>Nafakhum</h3>
+        <p>We had such a great time in NK!</p>
+      </div>
+    </div>
+  </div>
+  
+  <!-- Left and right controls -->
+         <a class="carousel-control-prev" href="#demo" data-slide="prev">
+              <span class="carousel-control-prev-icon"></span>
+         </a>
+         <a class="carousel-control-next" href="#demo" data-slide="next">
+              <span class="carousel-control-next-icon"></span>
+         </a>
+  </div>
+      <section class="my-5">
+           <div class="py-5">
+              <h2 class="text-center text-success"> Bandarban </h2>
+           </div>
+        <div class="container-fluid">
+            <div class="row">
+               <div class="col-lg-6 col-md-6 col-12">
+                   <img src="images/tajindong peak.jpg" class="img-fluid aboutimg">     
+               </div>
+               <div class="col-lg-6 col-md-6 col-12">
+                   <h2> Hello Bandarban </h2> 
+                      <p class="py-3">Bandarban is regarded as one of the most attractive travel destinations in Bangladesh.
+                       It also is the administrative headquarters of Bandarban district, which has turned into 
+                       one of the most exotic tourist attractions in Bangladesh. </p>
+                       <p class="py-3"> Bandarban district was established in 1983.It consists of 7 upazilas,32 unions,140 mouzas,
+                       1482 villages, 1 municipality, 10 wards and 62 mahallas. Bandarban , also called Rwadawmro in 
+                       the Marma and Arakanese languages, is a district in South-Eastern Bangladesh, and a part of 
+                       the Chittagong Division. It is one of the three hill districts of Bangladesh . </p>
+                       <p class="py-3"> There are 28 tourist attractions found in Bandarban. the top tourists spots are keokradong
+                       pahar, amiakum zhorna, nafakhum, alikadam (dim pahar),Chimbuk pahar, Nilachol, Bogalake, Nilgiri, 
+                       sat vai khum, thanchi, debota pahar etc. </P>
+                       <a href="about.php" class="btn btn-success"> Check More </a>  
+               </div>
+            </div>
+        </div>
+      </section> 
+
+      <section class="my-5">
+           <div class="py-4">
+              <h2 class="text-center text-success"> Attractive spots </h2>
+           </div>
+           <div class="contain-fluid">
+               <div class="row">
+                   <div class="col-lg-4 col-md-4 col-12">
+                      <div class="card">
+            <img class="card-img-top" src="images/thanchi.jpg" alt="Card image">
+                           <div class="card-body">
+                             <h4 class="card-title">Thanchi</h4>
+                              <p class="card-text">The popular Place.One such path leads to it .One of the remotest parts of our country. if u visit here you'll enjoy.</p>
+                              <a href="about.php" class="btn btn-primary">See More</a>
+                           </div>
+                      </div>
+                   </div>
+                   <div class="col-lg-4 col-md-4 col-12">
+                      <div class="card">
+            <img class="card-img-top" src="images/amiakhum112.jpg" alt="Card image">
+                           <div class="card-body">
+                             <h4 class="card-title">Amiakhum Zhorna</h4>
+                              <p class="card-text">  It is one of the most inaccessible places  in  Bangladesh.
+                              A  stream  of  cool water is   coming down at a
+                              tremendous speed, spreading milky  white  foam   over the green hills. just a beautiful place </p>
+                              <a href="about.php" class="btn btn-primary">See More</a>
+                           </div>
+                      </div>
+                   </div>
+                   <div class="col-lg-4 col-md-4 col-12">
+                      <div class="card">
+            <img class="card-img-top" src="images/nafakhum3.jpg" alt="Card image">
+                           <div class="card-body">
+                             <h4 class="card-title">Nafakhum Zhorna </h4>
+                              <p class="card-text">Sagu the river which flowing exotic waterfall call Nafakhum. It is among the largest waterfalls in the country by volume of water falling.</p>
+                              <a href="about.php" class="btn btn-primary">See More</a>
+                           </div>
+                      </div>
+                   </div>
+               </div>
+           </div> 
+       </section>
+   
+       <section class="my-5">
+             <div class="py-4">
+                <h2 class="text-center text-success"> Gallery </h2>
+             </div>
+               <div class="contain-fluid">
+                 <div class="row">
+                   <div class="col-lg-4 col-md-4 col-12"> 
+                   <h5 class="card-text-"> Amiakhum Jhorna</h5> 
+                      <img src="images/amiakhum112.jpg" class="img-fluid pb-3">  
+                   </div> 
+                   <div class="col-lg-4 col-md-4 col-12">  
+                   <h5 class="card-text-"> Nilachol</h5>    
+                   <img src="images/nilachol114.jpg" class="img-fluid pb-3 ">
+                   </div>
+
+                   <div class="col-lg-4 col-md-4 col-12">
+                   <h5 class="card-text-"> Royado Rong </h5>   
+                      <img src="images/royadorong117.jpg" class="img-fluid pb-3">  
+                   </div>
+
+                  <div class="col-lg-4 col-md-4 col-12">  
+                   <h5 class="card-text-"> Nafakhum Jhorna</h5> 
+                      <img src="images/nafakhumjhorna120.jpg" class="img-fluid pb-3">  
+                   </div>
+
+                   <div class="col-lg-4 col-md-4 col-12">
+                   <h5 class="card-text-"> Nilgiri</h5>   
+                      <img src="images/nilgiri116.jpg" class="img-fluid pb-3">  
+                   </div>
+
+                   <div class="col-lg-4 col-md-4 col-12">
+                   <h5 class="card-text-"> Tajindong </h5>  
+                      <img src="images/tajindong peak.jpg" class="img-fluid pb-3">  
+                   </div>
+
+                   <div class="col-lg-4 col-md-4 col-12">  
+                   <h5 class="card-text-"> Sunset Time </h5> 
+                      <img src="images/sunsettimebandorban118.jpg" class="img-fluid pb-3">  
+                   </div>
+
+                   <div class="col-lg-4 col-md-4 col-12"> 
+                   <h5 class="card-text-"> Unknown Hill</h5>  
+                      <img src="images/bandorban115.jpg" class="img-fluid pb-3">  
+                   </div>
+
+                   <div class="col-lg-4 col-md-4 col-12">  
+                   <h5 class="card-text-"> Tolabong Jhorna</h5> 
+                      <img src="images/tolabongzhorna119.jpg" class="img-fluid pb-3">  
+                   </div>
+                   
+                   
+                 </div>
+               </div>
+
+        </section>
+
+        <section class="my-5">
+             <div class="py-5">
+                <h2 class="text-center text-success"> Contact Us </h2>
+             </div>
+                <div class="w-50 m-auto">
+                    <form action="userinfo.php" method="post">
+                       <div class="form-group">
+                         <label> Username </label>
+                         <input type="text" name="user" autocomplete="off" class="form-control">
+                       </div>
+                       <div class="form-group">
+                         <label> Email  Id</label>
+                         <input type="text" name="email" autocomplete="off" class="form-control">
+                       </div>
+                       <div class="form-group">
+                         <label> Mobile</label>
+                         <input type="text" name="mobile" autocomplete="off"  class="form-control">
+                       </div>
+                       <div class="form-group">
+                           <label> Comment </label>
+                           <textarea class="form-control" name="comment"> 
+
+                           </textarea>
+                       </div>
+                       <button type="submit" class="btn btn-success">Submit</button>
+                    </form>
+                </div>     
+        </section>
+
+        <footer>
+          <p class="p-3 bg-dark text-white text-center"> @planetplus</p>
+        </footer>
+
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+</body>
+</html>
+
